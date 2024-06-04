3fccc6975ff620a372c2ab09d809dd1553047d58
AndresP115 <p.andres0115@gmail.com>
2024-06-04 17:23:46 -0500

    Second commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+
+    // Asignar eventos
+    // Cuando el usuario sale de un campo a otro
+    inputEmail.addEventListener('blur', validar );
+    inputAsunto.addEventListener('blur', validar );
+    inputMensaje.addEventListener('blur', validar );
+
+    function validar(e) {
+        if(e.target.value.trim() === '') {
+            mostrarAlerta();
+        } else {
+            console.log('Si hay algo');
+        }
+    }
+
+    //Mostrar altera
+    function mostrarAlerta() {
+        // Generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = 'Hubo un error....';
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center'); // Tailwind-css
+
+        // Inyectar el error en el formulario
+        formulario.appendChild(error);
+    }
+
+});
+
+//trim() Elimina todos los espacios en blanco
 
 
 
