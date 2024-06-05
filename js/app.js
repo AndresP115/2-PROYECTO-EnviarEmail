2f642fd1131467349171cc5dc7054b7ff92c00c9
AndresP115 <p.andres0115@gmail.com>
2024-06-05 14:01:49 -0500

    Third commit

--- js/app.js
+++ js/app.js
@@ -14,23 +14,49 @@ document.addEventListener('DOMContentLoaded', function() {
     inputMensaje.addEventListener('blur', validar );
 
     function validar(e) {
+        console.log()
         if(e.target.value.trim() === '') {
-            mostrarAlerta();
-        } else {
-            console.log('Si hay algo');
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return;
         }
+
+        if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('El imail no es válido', e.target.parentElement);
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
     }
 
     //Mostrar altera
-    function mostrarAlerta() {
+    function mostrarAlerta(mensaje, referencia) {
+        limpiarAlerta(referencia)
+       
+
         // Generar alerta en HTML
         const error = document.createElement('P');
-        error.textContent = 'Hubo un error....';
+        error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center'); // Tailwind-css
 
         // Inyectar el error en el formulario
-        formulario.appendChild(error);
+        referencia.appendChild(error);
     }
+
+    function limpiarAlerta(referencia) {
+        //Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email) {
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email)
+        return resultado
+    }
+
+
 
 });
 
